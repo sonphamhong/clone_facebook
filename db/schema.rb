a74d68862b0465835f3b39e30b0d444192f3bad7
Sonph <evilsp4@gmail.com>
2015-05-11 21:50:44 +0700

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -74,10 +74,38 @@ ActiveRecord::Schema.define(:version => 20130507184210) do
   add_index "mentions", ["mentionable_id", "mentionable_type"], :name => "fk_mentionables"
   add_index "mentions", ["mentioner_id", "mentioner_type"], :name => "fk_mentions"
 
-# Could not dump table "posts" because of following StandardError
-#   Unknown type 'attachment' for column 'avatar'
+  create_table "posts", :force => true do |t|
+    t.integer  "user_id"
+    t.text     "content"
+    t.datetime "created_at",          :null => false
+    t.datetime "updated_at",          :null => false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
+  end
 
-# Could not dump table "users" because of following StandardError
-#   Unknown type 'attachment' for column 'avatar'
+  create_table "users", :force => true do |t|
+    t.string   "email",                                 :default => "", :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                         :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                                            :null => false
+    t.datetime "updated_at",                                            :null => false
+    t.string   "name"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
